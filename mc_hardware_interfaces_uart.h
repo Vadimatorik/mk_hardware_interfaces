42a463e7e4f174f0bb3b6242c86add7ba9a16894
Vadimatorik <Deryabkinda@mail.ru>
2018-01-08 02:12:50 +0700

    В модуль uart добавлена защита от включения в .h.

--- mc_hardware_interfaces_uart.h
+++ mc_hardware_interfaces_uart.h
@@ -2,6 +2,8 @@
 
 #include "mc_hardware_interfaces_base.h"
 
+#ifdef __cplusplus
+
 class uart_base {
 public:
 	virtual void 			reinit		( void ) const = 0;
@@ -12,3 +14,5 @@ public:
     virtual BASE_RESULT		tx			( const uint8_t* const  p_array_tx, const uint16_t& length, const uint32_t& timeout_ms ) const = 0;
     virtual BASE_RESULT		get_byte	( uint8_t* data ) const = 0;
 };
+
+#endif
