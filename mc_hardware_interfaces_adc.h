b78ea41677d4edb3a4c86b0a902aca84e4b88992
Vadimatorik <Deryabkinda@mail.ru>
2018-03-10 18:05:35 +0700

    Добавлено возвращаемое значение у adc методов и timer методов.

--- mc_hardware_interfaces_adc.h
+++ mc_hardware_interfaces_adc.h
@@ -2,15 +2,6 @@
 
 #include <stdint.h>
 
-namespace SPI {
-
-enum class FUNC_RESULT {
-    OK                          = 0,            // Операция успешна произведена.
-    MEASUREMENT_IS_MISSING      = 1,            // Измерение отсутствует.
-};
-
-}
-
 class adc_one_channel_base {
 public:
     //**********************************************************************
@@ -20,23 +11,24 @@ public:
     // (на один объект один канал и 1 ражим).
     // После переинициализации ADC должен остаться отключенным.
     //**********************************************************************
-    virtual void reinit ( void ) const = 0;
+	virtual bool reinit ( void ) const = 0;
 
     //**********************************************************************
     // Методы должены включать/отключать ADC (не перетерая поля регистров).
     //**********************************************************************
-    virtual void on     ( void ) const = 0;
-    virtual void off    ( void ) const = 0;
+	virtual void clk_enable     ( void ) const = 0;
+	virtual void clk_disable	( void ) const = 0;
 
     //**********************************************************************
     // Метод должен инициировать новое измерение и выйти,
     // не дожидаясь его окончания.
     //**********************************************************************
-    virtual void start_continuous_conversion ( void ) const = 0;
+	virtual bool start_continuous_conversion ( void )	const = 0;
+	virtual bool stop_continuous_conversion ( void )	const = 0;
 
     //**********************************************************************
     // Метод получает результат измерения (если таковой доступен).
     // В противном случае сообщает о неудаче.
     //**********************************************************************
-    virtual SPI::FUNC_RESULT get_measurement ( uint32_t& channel_measurement ) const = 0;
+	virtual void get_measurement ( uint32_t& channel_measurement ) const = 0;
 };
