e7e7b1838724474302b8dda4aaa5fb9aadd477a1
Vadimatorik <deryabkinda@mail.ru>
2018-04-15 17:09:34 +0700

    Добавил DAC, поправил UART.

--- mc_hardware_interfaces_uart.h
+++ mc_hardware_interfaces_uart.h
@@ -4,15 +4,18 @@
 
 #ifdef __cplusplus
 
-class uart_base {
+class UartBase {
 public:
 	virtual bool 			reinit		( void ) const = 0;
 
 	virtual void 			on     		( void ) const = 0;
 	virtual void 			off    		( void ) const = 0;
 
-    virtual BASE_RESULT		tx			( const uint8_t* const  p_array_tx, const uint16_t& length, const uint32_t& timeout_ms ) const = 0;
-    virtual BASE_RESULT		get_byte	( uint8_t* data ) const = 0;
+    virtual BASE_RESULT		tx			(	const uint8_t* const  p_array_tx,
+    										const uint16_t& length,
+											const uint32_t& timeout_ms	)		const = 0;
+
+    virtual BASE_RESULT		getByte		( uint8_t* data )						const = 0;
 };
 
 #endif
