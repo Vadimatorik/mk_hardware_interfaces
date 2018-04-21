861ac931964c12c7d2bbf003523041d2ca9e31c9
Vadimatorik <deryabkinda@mail.ru>
2018-04-21 11:25:43 +0700

    Оформил в doxygen mc_hardware_interfaces_base.h.

--- mc_hardware_interfaces_base.h
+++ mc_hardware_interfaces_base.h
@@ -4,14 +4,24 @@
 
 #ifdef __cplusplus
 
+/*!
+ * Стандартные ответы от методов
+ * интерфейсов микроконтроллера.
+ */
 enum class BASE_RESULT {
-	OK                  = 0,            // Операция успешна произведена.
-    TIME_OUT            = 1,            // Был произведён выход по истечении времени ожидания.
-    LENGTH_ERROR        = 2,            // Попытка передать/принять пакет 0-й длины.
-    ERROR_INIT          = 3,
-	NOT_DATA			= 4,			// Нет запрашиваемых данных.
+	OK					=	0,			/// Операция успешна произведена.
+	TIME_OUT			=	1,			/// Был произведён выход по истечении времени ожидания.
+	LENGTH_ERROR		=	2,			/// Попытка передать/принять пакет 0-й длины.
+	ERROR_INIT			=	3,			/// Ошибка инициализации/модуль не был ранее инициализирован.
+	NOT_DATA			=	4,			/// Нет запрашиваемых данных.
 };
 
-#define check_result( base_result_variable )			if ( base_result_variable != BASE_RESULT::OK ) return base_result_variable;
+/*!
+ * Шаблон позволяет прекратить последовательность
+ * действий и выйти, если операция с интерфейсом
+ * была произведена не удачно.
+ */
+#define	checkResult( baseResultVariable )											\
+		if ( baseResultVariable != BASE_RESULT::OK ) return baseResultVariable;
 
 #endif
